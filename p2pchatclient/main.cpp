2bd36f86529716b02f85baac89b3b23edbb4500a
Axel Vanherle <axelvanherle@outlook.com>
2023-02-20 20:45:44 +0100

    Added gui and small tcpclient fix

--- p2pchatclient/main.cpp
+++ p2pchatclient/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <QtWidgets>
 
 #include "tcpclient.h"
 
@@ -18,30 +19,69 @@ int main(int argc, char *argv[])
 
     TcpClient client;
 
+    // Create main window
+    QWidget window;
+    window.setWindowTitle("P2P-TCP-chatclient-NP by Axel Vanherle.");
+    window.setFixedSize(400, 600);
+
+    // Create widgets
+    QLabel* outputLabel = new QLabel("Enter message:");
+    QLineEdit* inputLineEdit = new QLineEdit();
+    QPushButton* sendButton = new QPushButton("Send");
+    QLabel* inputLabel = new QLabel("Received messages:");
+    QPlainTextEdit* receivedTextEdit = new QPlainTextEdit();
+    receivedTextEdit->setReadOnly(true);
+    receivedTextEdit->setMaximumBlockCount(100);
+    QLabel* debugLabel = new QLabel("Debug messages:");
+    QPlainTextEdit* debugTextEdit = new QPlainTextEdit();
+    debugTextEdit->setReadOnly(true);
+    debugTextEdit->setMaximumHeight(110);
+    debugTextEdit->setMaximumBlockCount(6);
+
+    // Create layout
+    QVBoxLayout* layout = new QVBoxLayout();
+    layout->addWidget(outputLabel);
+    layout->addWidget(inputLineEdit);
+    layout->addWidget(sendButton);
+    layout->addWidget(inputLabel);
+    layout->addWidget(receivedTextEdit);
+    layout->addWidget(debugLabel);
+    layout->addWidget(debugTextEdit);
+
+    window.setLayout(layout);
+
     if (argc == 3)
         client.firstConnect(argv[1], atoi(argv[2]));
 
     QObject::connect(&client, &TcpClient::newConnection, [&](QTcpSocket *socket)
     {
         qDebug() << "New connection from: " << socket->peerAddress().toString();
+        debugTextEdit->appendPlainText("New connection from: " + socket->peerAddress().toString());
         QObject::connect(socket, &QTcpSocket::readyRead, &client, &TcpClient::readFromAll);
     });
 
     QObject::connect(&client, &TcpClient::newMessageReceived, [&](QString message)
     {
         qDebug() << "New message received: " << message;
+        receivedTextEdit->appendPlainText(message);
     });
 
-    while (1)
+    QObject::connect(sendButton, &QPushButton::clicked, [&]()
     {
-        QString message = QInputDialog::getText(nullptr, "Send message", "Enter message:");
+        QString message = inputLineEdit->text();
         if (message.isEmpty())
         {
-            continue;
+            // Show message box with error
+            QMessageBox::warning(&window, "Error", "Message cannot be empty!");
+            return;
         }
         qDebug() << "Message sent: " << message;
+        debugTextEdit->appendPlainText("Message sent: " + message);
         client.sendToAll(message);
-    }
+        inputLineEdit->clear();
+    });
 
+    // Show window
+    window.show();
     return a.exec();
 }
